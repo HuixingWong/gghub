18813eaeb91d4fc8c6142317c2294db06057f0ae
HuixingWang <huixing.wang@storehub.com>
2020-08-24 18:33:08 +0800

    remove unusse code

--- app/src/main/java/com/qingmei2/sample/utils/ContextExt.kt
+++ app/src/main/java/com/qingmei2/sample/utils/ContextExt.kt
@@ -5,11 +5,6 @@ import android.content.Intent
 import com.qingmei2.sample.ui.web.CommonWebViewActivity
 
 fun Context.jumpBrowser(url: String) {
-//    val uri = Uri.parse(url)
-//    Intent(Intent.ACTION_VIEW, uri).apply {
-//        flags = Intent.FLAG_ACTIVITY_NEW_TASK
-//        startActivity(this)
-//    }
     Intent(this, CommonWebViewActivity::class.java).apply {
         putExtra("url", url)
         startActivity(this)
