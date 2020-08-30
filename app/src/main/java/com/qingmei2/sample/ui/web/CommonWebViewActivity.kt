12f913c93d318bbc73a6baefce469d1963829528
HuixingWang <huixing.wang@storehub.com>
2020-08-30 17:48:55 +0800

    webView theme

--- app/src/main/java/com/qingmei2/sample/ui/web/CommonWebViewActivity.kt
+++ app/src/main/java/com/qingmei2/sample/ui/web/CommonWebViewActivity.kt
@@ -1,8 +1,14 @@
 package com.qingmei2.sample.ui.web
 
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.core.content.ContextCompat
 import com.qingmei2.sample.R
+import com.qingmei2.sample.utils.isDarkTheme
 import kotlinx.android.synthetic.main.activity_common_web_view.*
 
 class CommonWebViewActivity : AppCompatActivity() {
@@ -11,6 +17,22 @@ class CommonWebViewActivity : AppCompatActivity() {
         setContentView(R.layout.activity_common_web_view)
         intent.extras?.getString("url")?.let {
             webview.loadUrl(it)
+        }
+        if (isDarkTheme()){
+            webview.setBackgroundColor(ContextCompat.getColor(this,android.R.color.transparent));
+            webview.setBackgroundResource(android.R.color.black)
+        }
+        webview.webViewClient = object : WebViewClient(){
+            override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+                super.onPageStarted(view, url, favicon)
+                mLoading.visibility = View.VISIBLE
+            }
+
+            override fun onPageFinished(view: WebView?, url: String?) {
+                super.onPageFinished(view, url)
+                mLoading.visibility = View.GONE
+            }
+
         }
     }
 }
